5f7c03918ef2dd066048bf8d21f0d8245df98592
Evandro Avellar <evandro.avellar@gmail.com>
2023-08-23 08:47:11 -0300

    Novo arquivo

---
+++ aulas/aula5_arrays_repeticao.js
@@ -0,0 +1,30 @@
+// Listas ou Arrays
+
+const lista = [1, 2, 3, 'Nome'];
+
+
+console.log(lista[3]);
+
+// Adicionar itens
+
+// Na última posição
+lista.push('Evandro');
+
+console.log(lista);
+
+// Substituindo um valor
+lista[2] = 'Novo';
+
+console.log(lista);
+
+// Apagar o último
+
+lista.pop();
+
+console.log(lista);
+
+// Apagar o primeiro
+
+lista.shift();
+
+console.log(lista);
